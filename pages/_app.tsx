fc152b1663c004fe86ab70b11fd97a932a257a92
Jason <jasondal91@gmail.com>
2023-06-29 22:37:41 +0900

    chore: pages폴더로 구조 변경 및 globalstyles 적용

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from 'next/app';
+import { Global } from '@emotion/react';
+import { globalStyles } from '../styles/globalStyle';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </div>
+  );
+}
