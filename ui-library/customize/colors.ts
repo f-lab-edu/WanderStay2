e7c6407923c69d771915fbbb433c429dcc71d48b
choihansu97 <jjpp9712@naver.com>
2023-06-30 10:06:36 +0900

    feat: 디자인 시스템 색상값 추가

---
+++ ui-library/customize/colors.ts
@@ -0,0 +1,81 @@
+export const brand = {
+    brand100: '#FEF9D8',
+    brand200: '#FEF3B2',
+    brand300: '#FEE98C',
+    brand400: '#FDE06F',
+    brand500: '#FCD240',
+    brand600: '#D8AE2E',
+    brand700: '#B58D20',
+    brand800: '#926D14',
+    brand900: '#78560C',
+}
+
+export const black = {
+    black0  : '#ffffff',
+    black100: '#F3F3F3',
+    black200: '#E8E8E8',
+    black300: '#BABABA',
+    black400: '#767676',
+    black500: '#1B1B1B',
+    black600: '#171314',
+    black700: '#130D0E',
+    black800: '#0F080A',
+    black900: '#0C0507',
+}
+
+export const success = {
+    success100: '#D1FCDA',
+    success200: '#A5F9BF',
+    success300: '#76EFA8',
+    success400: '#52E09B',
+    success500: '#20CC89',
+    success600: '#17AF83',
+    success700: '#109279',
+    success800: '#0A766C',
+    success900: '#066161',
+}
+
+export const error = {
+    error100: '#FEEAD2',
+    error200: '#FED0A5',
+    error300: '#FEAF78',
+    error400: '#FD8F56',
+    error500: '#FD5B1F',
+    error600: '#D93D16',
+    error700: '#B6250F',
+    error800: '#921209',
+    error900: '#790506',
+}
+
+export const information = {
+    information100: '#D0E7FF',
+    information200: '#A1CCFF',
+    information300: '#73AEFF',
+    information400: '#5094FF',
+    information500: '#1668FF',
+    information600: '#1050DB',
+    information700: '#0B3BB7',
+    information800: '#072993',
+    information900: '#041C7A',
+}
+
+const headLine = {
+    size100 : '12px',
+    size200 : '14px',
+    size300 : '16px',
+    size400 : '20px',
+    size500 : '24px',
+    size700 : '36px',
+    size800 : '48px',
+}
+
+const heading = {
+    size100 : '10px',
+    size200 : '12px',
+}
+
+const bodyText = {
+    size100 : '12px',
+    size200 : '14px',
+    size300 : '16px',
+}
