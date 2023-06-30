e2eecff736ea21ede6b6cf8f5a567b4e404a72d8
choihansu97 <jjpp9712@naver.com>
2023-06-30 10:07:17 +0900

    feat: 디자인 시스템 헤드 추가

---
+++ ui-library/typography/headings.ts
@@ -0,0 +1,14 @@
+export const headLine = {
+    size100: '12px',
+    size200: '14px',
+    size300: '16px',
+    size400: '20px',
+    size500: '24px',
+    size700: '36px',
+    size800: '48px',
+};
+
+export const heading = {
+    size100: '10px',
+    size200: '12px',
+};
