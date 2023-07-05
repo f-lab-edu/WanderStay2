f5ea6779b3852f2cf4ffd02e0e493caf28fee256
choihansu97 <jjpp9712@naver.com>
2023-07-06 01:08:54 +0900

    feat: 누락된 headline, bodyText 추가

--- ui-library/typography/typography.ts
+++ ui-library/typography/typography.ts
@@ -3,11 +3,17 @@ import {fontSizes} from "@/ui-library/typography/fontSizes";
 import {fontWeights} from "@/ui-library/typography/fontWeights";
 import {lineHeights} from "@/ui-library/typography/lineHeights";
 import {letterSpacings} from "@/ui-library/typography/letterSpacings";
+import {headline} from "@/ui-library/typography/headline";
+import {heading} from "@/ui-library/typography/heading";
+import {bodyText} from "@/ui-library/typography/bodyText";
 
 export const typography = {
     fontFamilys,
     fontSizes,
     fontWeights,
     lineHeights,
-    letterSpacings
+    letterSpacings,
+    headline,
+    heading,
+    bodyText,
 };
