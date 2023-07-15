2ab4237f0939d3d54059c7bb9d8e6aa76f9b9cba
Jason <jasondal91@gmail.com>
2023-07-15 12:35:01 +0900

    fix: test input 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Input } from '@/components/commons/Input';
+
 export default function Home() {
   return (
     <>
       <div>Home</div>
-      <Input />
     </>
   );
 }
