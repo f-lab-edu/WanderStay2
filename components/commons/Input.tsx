f4b1def8af66af42cbac7b066829ae25bfd4d4fd
Jason <jasondal91@gmail.com>
2023-07-17 08:18:44 +0900

    feat: 공통 Input component style 추가

--- components/commons/Input.tsx
+++ components/commons/Input.tsx
@@ -1,27 +1,55 @@
 import styled from '@emotion/styled';
+import inputStyle from '@/ui-library/customize/input';
+import InputTitle from './InputTitle';
 
-interface InputProps extends React.ComponentProps<'input'> {
+export interface InputComponentProps extends React.ComponentProps<'input'> {
   inputBoxSize: 'sm' | 'lg';
+  inputTitle: string;
   isHide: boolean;
+  category: Extract<
+    React.HTMLInputTypeAttribute,
+    'text' | 'password' | 'search'
+  >;
 }
-type InputComponentProps = Partial<InputProps>;
-type WapperProps = Pick<InputProps, 'inputBoxSize' | 'isHide' | 'type'>;
+
+type InputProps = Partial<InputComponentProps>;
+
+type WapperProps = Pick<
+  InputComponentProps,
+  'inputBoxSize' | 'isHide' | 'category'
+>;
 
 export const Input = ({
-  type = 'text',
   inputBoxSize = 'lg',
   isHide = false,
+  title,
+  category = 'text',
   ...props
-}: InputComponentProps) => {
+}: InputProps) => {
   return (
-    <Wrapper
-      inputBoxSize={inputBoxSize}
-      isHide={isHide}
-      type={type}
-      value={props.value}
-      onChange={props.onChange}
-    ></Wrapper>
+    <Wrapper>
+      <InputTitle>{title}</InputTitle>
+      <InputWrapper
+        inputBoxSize={inputBoxSize}
+        isHide={isHide}
+        category={category}
+        type={props.type}
+        size={props.size}
+        value={props.value}
+        onChange={props.onChange}
+        style={props.style}
+      ></InputWrapper>
+    </Wrapper>
   );
 };
 
-const Wrapper = styled.input<WapperProps>((props) => ({}));
+const InputWrapper = styled.input<WapperProps>((props) => ({
+  ...inputStyle.default,
+  ...inputStyle.category[props.category],
+}));
+
+const Wrapper = styled.div({
+  display: 'inline-block',
+  margin: '12px',
+  position: 'relative',
+});
