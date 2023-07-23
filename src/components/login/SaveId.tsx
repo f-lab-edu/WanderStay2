75750dfd1f459b1300fd624a024c4c21d12b1faa
Jason <jasondal91@gmail.com>
2023-07-23 21:27:01 +0900

    fix: input style 복구

--- src/components/login/SaveId.tsx
+++ src/components/login/SaveId.tsx
@@ -4,9 +4,7 @@ import { theme } from '@/src/ui-library/theme';
 export default function SaveId() {
   return (
     <Wrapper>
-      <CheckBoxWrapper>
-        <HiddenCheckbox type='checkbox' />
-      </CheckBoxWrapper>
+      <Checkbox type='checkbox' />
       <SaveIdText>아이디 저장</SaveIdText>
     </Wrapper>
   );
@@ -17,30 +15,9 @@ const Wrapper = styled.div({
   alignContent: 'center',
 });
 
-const HiddenCheckbox = styled.input`
-  position: absolute;
-  overflow: hidden;
-  clip: rect(0 0 0 0);
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  border: 0;
-`;
-
-const CheckBoxWrapper = styled.label`
-  display: flex;
-  align-items: center;
-  height: 18px;
-  width: 18px;
-  border: 2px solid ${({ theme }) => theme.color.black.black100};
-  margin-right: 5px;
-  &:checked {
-    background-color: ${({ theme }) => theme.color.black.black0};
-    border-color: ${({ theme }) => theme.color.success.success600};
-    opacity: 1;
-  }
-`;
+const Checkbox = styled.input({
+  marginRight: '5px',
+});
 
 const SaveIdText = styled.span((props) => ({
   color: theme.color.black.black300,
