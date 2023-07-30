2159597a187867df28d203a78d5a6c1bdf48c48a
choihansu97 <60763027+choihansu97@users.noreply.github.com>
2023-07-30 14:57:51 +0900

    refactor : background 수정

--- ui-library/components/iconCategory/categoryIcon.tsx
+++ ui-library/components/iconCategory/categoryIcon.tsx
@@ -4,39 +4,41 @@ import styled from "@emotion/styled";
 
 interface CategoryIconProps {
     name: keyof typeof category;
-    size: 'small' | 'medium';
+    size: 'sm' | 'md';
 }
 
-const IconBackground = styled.div<CategoryIconProps>`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: ${props => (props.size === 'small' ? '44px' : '70px')};
-    height: ${props => (props.size === 'small' ? '44px' : '70px')};
-    padding: 10px;
-    background: #fafafa;
-    border-radius: ${props => (props.size === 'small' ? '15px' : '25px')};
+type CategoryIconSize = Pick<CategoryIconProps, 'size'>
+
+const IconBackground = styled.div<CategoryIconSize>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${props => (props.size === 'sm' ? '44px' : '70px')};
+  height: ${props => (props.size === 'sm' ? '44px' : '70px')};
+  padding: 10px;
+  background-color: #fafafa;
+  border-radius: ${props => (props.size === 'sm' ? '15px' : '25px')};
 `;
 
 const StyledImage = styled(Image)`
-    filter: drop-shadow(0px 4px 10px rgba(59, 125, 21, 0.3));
+  filter: drop-shadow(0px 4px 10px rgba(59, 125, 21, 0.3));
 `;
 
-const sizeMap: { [key in CategoryIconProps['size']]: number } = {
-    small: 24,
-    medium: 40
+const sizeMap: { [key in CategoryIconSize['size']]: number } = {
+    sm: 24,
+    md: 40
 };
 
-const CategoryIcon = ({name, size = 'small'}: CategoryIconProps) => {
+const CategoryIcon = ({name, size = 'sm'}: CategoryIconProps) => {
     if (!name in category) {
         throw new Error(`이름이 잘못 되었습니다.`);
     }
 
-    if (size !== 'small' && size !== 'medium') {
+    if (size !== 'sm' && size !== 'md') {
         throw new Error('입력하신 사이즈가 없습니다.');
     }
 
-    const iconSize = sizeMap[size] || sizeMap['small'];
+    const iconSize = sizeMap[size] || sizeMap['sm'];
 
     return (
         <IconBackground size={size}>
@@ -47,7 +49,9 @@ const CategoryIcon = ({name, size = 'small'}: CategoryIconProps) => {
                 height={iconSize}
                 quality={100}
                 placeholder="blur"
-                onError={(e) => console.log(e.target.name)}
+                onError={(e) => {
+                    throw new Error(e.target.name)
+                }}
             />
         </IconBackground>
     );
