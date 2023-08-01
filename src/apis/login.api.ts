0a54f5dcbb5852ce1522cb2149a7c7b2fae56c57
Jason <jasondal91@gmail.com>
2023-08-01 19:23:23 +0900

    feat: login api 추가

--- src/apis/login.api.ts
+++ src/apis/login.api.ts
@@ -1,8 +1,8 @@
 import httpClient from './httpClient';
 
-const LOGIN_URL = '/auth/login';
+const LOGIN_URL = '/api/login';
 const loginApi = async (body: any) => {
-  return await httpClient.post(LOGIN_URL, body, {
+  return httpClient.post(LOGIN_URL, body, {
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
   });
