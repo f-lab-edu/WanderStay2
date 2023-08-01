580d3aedd4be353651f7cc982e177fad01acfbe9
choihansu97 <jjpp9712@naver.com>
2023-08-01 16:36:33 +0900

    feat: 목적지 정보 컴포넌트 작업

--- src/components/destinationInformation/destinationInformation.tsx
+++ src/components/destinationInformation/destinationInformation.tsx
@@ -4,27 +4,31 @@ import Icon from '@/src/ui-library/components/icons/svg';
 import {theme} from "@/src/ui-library/theme";
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  column-gap: 5px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    column-gap: 5px;
 `
 
 const DestinationTitle = styled.span`
-  line-height: 25px;
-  color: white;
-  font-size: 12px;
-  font-weight: 400;
-  letter-spacing: 0.12px;
+    line-height: 25px;
+    color: white;
+    font-size: 12px;
+    font-weight: 400;
+    letter-spacing: 0.12px;
 `
 
-const DestinationInformation = () => {
+const DestinationInformation = (props) => {
     return (
         <Wrapper>
             <Icon
                 name={"Location"}
+                width={16}
+                height={16}
+                viewBox={'0 0 16 16'}
                 color={theme.color.black.black0}
             />
-            <DestinationTitle>여행지명</DestinationTitle>
+            <DestinationTitle>{props.destinationInfo}</DestinationTitle>
         </Wrapper>
     )
 }
